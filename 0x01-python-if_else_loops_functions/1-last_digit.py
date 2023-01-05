71b5bc39bb21259cb2a347c762b80b516a522533
Super User <okoliogochukwu83@gmail.com>
2023-01-05 00:23:56 -0800

    update

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,8 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-    num = number * -1
-    x = num % 10
+    x = number % -10
 else:
     x = number % 10
 if x > 5:
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,4 @@
 y = ''
 for x in range(97, 123):
     y += chr(x)
-    print(y[0:4] + y[5:16] + y[17:26], end="")
+print(y[0:4] + y[5:16] + y[17:26], end="")
