8f2619825cb7146a96e39247d9cf339cd71c1ac2
Ritalexi <okoliogochukwu83@gmail.com>
2023-02-02 03:57:44 -0800

    update

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+ A function that prints My name is <first name> <last name>
+ """
+
+
+ def say_my_name(first_name, last_name=""):
+     """
+     Print the name 
+     Args:
+     first_name - String  first name.
+     last_name  - String  last name.
+     raise:
+     raise type error if parameter are not 
+     string else print name
+     """
+
+     if not isinstance(first_name, str):
+         raise TypeError("first_name must be a string")
+     if not isinstance(last_name, str):
+         raise TypeError("last_name must be a string")
+     print("My name is {} {}".format(first_name, last_name))
---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+A function that prints a square with the
+character #.
+"""
+
+
+def print_square(size):
+    """
+    Print a square with the # character.
+    size - int 
+    If size is not an integer type error and 
+    if size is < 0 value error 
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        [print("#", end="") for j in range(size)]
+        print("")
