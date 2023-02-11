182e869b9542e3ee4d46d646bb3dbdc95b8d6bd3
Ritalexi <okoliogochukwu83@gmail.com>
2023-02-11 04:46:51 -0800

    update

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module for from_json_string method.
+"""
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    Module for from_json_string method.
+    """
+    return (json.loads(my_str))
